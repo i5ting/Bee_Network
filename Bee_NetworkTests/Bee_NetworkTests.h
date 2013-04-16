38b27398e014a81445208b9db510e11be0105a08
i5ting <i5ting@126.com>
2013-04-16 13:23:58 +0800

    Initial Commit

---
+++ Bee_NetworkTests/Bee_NetworkTests.h
@@ -0,0 +1,13 @@
+//
+//  Bee_NetworkTests.h
+//  Bee_NetworkTests
+//
+//  Created by sang on 4/16/13.
+//  Copyright (c) 2013 alfred sang. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Bee_NetworkTests : SenTestCase
+
+@end
---
+++ Bee_NetworkTests/Bee_NetworkTests.m
@@ -0,0 +1,32 @@
+//
+//  Bee_NetworkTests.m
+//  Bee_NetworkTests
+//
+//  Created by sang on 4/16/13.
+//  Copyright (c) 2013 alfred sang. All rights reserved.
+//
+
+#import "Bee_NetworkTests.h"
+
+@implementation Bee_NetworkTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Bee_NetworkTests");
+}
+
+@end
